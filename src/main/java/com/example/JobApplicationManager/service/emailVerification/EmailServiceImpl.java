78b748d19d53fd71d6b7f93a9b47389512dc3c1e
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-03-27 19:41:51 -0400

    dependency injection for JavaMailSender

--- src/main/java/com/example/JobApplicationManager/service/emailVerification/EmailServiceImpl.java
+++ src/main/java/com/example/JobApplicationManager/service/emailVerification/EmailServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.JobApplicationManager.service.emailVerification;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -11,8 +10,11 @@ import java.util.Properties;
 @Component
 public class EmailServiceImpl{
 
-    @Autowired
-    private JavaMailSender emailSender;
+    private final JavaMailSender javaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     public void sendSimpleMessage(
             String to, String subject, String text) {
@@ -21,7 +23,7 @@ public class EmailServiceImpl{
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        emailSender.send(message);
+        javaMailSender.send(message);
     }
 
 
