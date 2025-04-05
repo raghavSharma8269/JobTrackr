983e349faff6058ed1f1915afb5796cb6d2a53cc
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-04-05 15:18:24 -0400

    changed style for FilterByDropdownComponent.tsx

--- frontend/JobTrackrFrontend/src/components/NavBarComponents/FilterByDropdownComponent.tsx
+++ frontend/JobTrackrFrontend/src/components/NavBarComponents/FilterByDropdownComponent.tsx
@@ -2,40 +2,21 @@ import React from 'react'
 
 const FilterByDropdownComponent = () => {
     return (
-        <div className="dropdown">
-            <button
-                className="btn dropdown-toggle default-text-color"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{backgroundColor:"#7400f0", marginLeft:"10px"}}
-
-
-            >
-                Filter By...
+        <div className="dropdown" style={{marginLeft:"10px"}}>
+            <button className="btn  dropdown-toggle text-start" type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+            style={{backgroundColor:"#7400f0", color:"#c9c9c9"}}>
+                Filter...
             </button>
-            <ul className="dropdown-menu custom-filter-dropdown" style={{backgroundColor:"#292b38"}}>
-                <li>
-                    <a className="dropdown-item default-text-color" href="#">
-                        Applied
-                    </a>
-                </li>
-                <li>
-                    <a className="dropdown-item default-text-color" href="#">
-                        Interview
-                    </a>
-                </li>
-                <li>
-                    <a className="dropdown-item default-text-color" href="#">
-                        Accepted
-                    </a>
-                </li>
-                <li>
-                    <a className="dropdown-item default-text-color" href="#">
-                        Rejected
-                    </a>
-                </li>
+            <ul className="dropdown-menu" style={{backgroundColor:"#292b38"}}>
+                <li><a className="dropdown-item default-text-color" style={{backgroundColor:"blue", borderRadius:"10px"}} href="#">Applied</a></li>
+                <li><a className="dropdown-item default-text-color" style={{backgroundColor:"green", borderRadius:"10px"}} href="#">Interview</a></li>
+                <li><a className="dropdown-item default-text-color" style={{backgroundColor:"#7400f0", borderRadius:"10px"}} href="#">Accepted</a></li>
+                <li><a className="dropdown-item default-text-color" style={{backgroundColor:"red", borderRadius:"10px"}} href="#">Rejected</a></li>
             </ul>
+
+
         </div>
     )
 }
