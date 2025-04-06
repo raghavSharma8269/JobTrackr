c3de65400ac049820afdba509fffef9d26744421
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-04-06 12:44:11 -0400

    added button in AddJobModalComponent.tsx to add custom job

--- frontend/JobTrackrFrontend/src/components/NavBarComponents/AddJobModalComponent.tsx
+++ frontend/JobTrackrFrontend/src/components/NavBarComponents/AddJobModalComponent.tsx
@@ -4,39 +4,46 @@ const AddJobModalComponent = () => {
   return (
     <div
       className="modal fade default-text-color"
-      id="newJobModal" // No '#'
+      id="newJobModal"
       tabIndex={-1}
       aria-labelledby="newJobModalLabel"
       aria-hidden="true"
     >
       <div className="modal-dialog">
         <div className="modal-content">
+          {/* Modal Header */}
           <div className="modal-header light-bg">
             <h1 className="modal-title fs-5" id="newJobModalLabel">
               Add Job
             </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
-          <div className="modal-body light-bg">
-            <h3>Enter Job Via LinkedIn Link</h3>
 
-            <label htmlFor="jobLink" className="form-label">
-              Job Link
-            </label>
+          {/* Modal Body */}
+          <div className="modal-body light-bg">
+            <h3 className="mb-3">Enter Job Via LinkedIn Link</h3>
+
             <input
               type="text"
-              className="form-control light-bg"
+              className="form-control light-bg mb-4"
               id="jobLink"
               placeholder="Enter Job Link"
             />
-            <div className="modal-footer light-bg">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
+
+            <h5 className="text-center mb-3">OR</h5>
+
+            <div className="d-flex justify-content-center mb-4">
+              <button className="btn purple-bg default-text-color">
+                Enter Custom Job Details
               </button>
             </div>
+
+            <div className="d-flex justify-content-end"></div>
           </div>
         </div>
       </div>
