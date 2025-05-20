c58f1eb550384eaae964312031c3f8f2b7aad75b
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-05-20 18:22:54 -0400

    can now add jobs via linkedin

--- frontend/JobTrackrFrontend/src/components/NavBarComponents/AddJobModalComponent.tsx
+++ frontend/JobTrackrFrontend/src/components/NavBarComponents/AddJobModalComponent.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 import CustomJobModalComponent from "./CustomJobModalComponent.tsx";
+import axios from "axios";
 
 const AddJobModalComponent = () => {
+  const [jobUrl, setJobUrl] = React.useState("");
+
+  const handleAddingJobViaLinkedIn = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const request = {
+        jobUrl,
+      };
+
+      console.log("REQUEST DUDE: " + request.jobUrl);
+
+      await axios.post(
+        `http://localhost:8080/api/jobs/create?url=${encodeURIComponent(jobUrl)}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      // Reset form
+      setJobUrl("");
+
+      alert("Job added successfully!");
+    } catch (error) {
+      console.error("Error adding job:", error);
+
+      if (axios.isAxiosError(error) && error.response) {
+        const backendMessage = error.response.data;
+
+        if (backendMessage.message) {
+          alert(backendMessage.message);
+        }
+      } else {
+        alert("An unknown error occurred.");
+      }
+    }
+  };
+
   return (
     <>
       <div
@@ -36,11 +77,16 @@ const AddJobModalComponent = () => {
               <input
                 type="text"
                 className="form-control light-bg mb-4"
-                id="jobLink"
                 placeholder="Enter Job Link"
+                required
+                value={jobUrl}
+                onChange={(e) => setJobUrl(e.target.value)}
               />
               <div className="d-flex justify-content-center mb-4">
-                <button className="btn purple-bg default-text-color">
+                <button
+                  className="btn purple-bg default-text-color"
+                  onClick={handleAddingJobViaLinkedIn}
+                >
                   Add Job
                 </button>
               </div>
