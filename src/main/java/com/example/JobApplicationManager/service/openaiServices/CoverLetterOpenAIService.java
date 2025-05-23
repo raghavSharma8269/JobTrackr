40b54e29282535093aa552bc32ca17a7e9b086e2
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-05-23 11:41:28 -0400

    added logic to add cv feedback to jobs table

--- src/main/java/com/example/JobApplicationManager/service/openaiServices/CoverLetterOpenAIService.java
+++ src/main/java/com/example/JobApplicationManager/service/openaiServices/CoverLetterOpenAIService.java
@@ -74,6 +74,9 @@ public class CoverLetterOpenAIService {
 
         String updatedCoverLetter = callToOpenAiApi(resumeScannerService.execute(), job.getJobDescription(), job.getJobTitle(), job.getCompanyName(), coverLetterScannerService.execute());
 
+        job.setResumeFeedback(updatedCoverLetter);
+        jobsRepository.save(job);
+
         user.setNumOfAiRequests(user.getNumOfAiRequests() + 1);
         userRepository.save(user);
 
